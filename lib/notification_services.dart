4903a67014533ea0ce99316cb6b949887e9740e8
dharan-dharani <sdharandharani@gmail.com>
2025-10-16 23:33:17 +0530

    Update Gradle configurations and remove unused imports in notification models and services

--- lib/notification_services.dart
+++ lib/notification_services.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:html';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
