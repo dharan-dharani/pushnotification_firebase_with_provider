09beb8d5ff802b606b36a06b38fceb463d041443
dharan-dharani <sdharandharani@gmail.com>
2025-10-16 17:33:48 +0530

    Implement background message handling and initialize local notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:pushnotification_firebase_with_provider/notification_services.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message)async{
+  await Firebase.initializeApp();
+  await NotificationService.saveMessageToFirestoreFromBackground(message);
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await NotificationService.initializeLocalNotifications();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+@override
+void initState(){
+  super.initState();
+  NotificationService.configureFCM();
+}
 
   // This widget is the root of your application.
   @override
